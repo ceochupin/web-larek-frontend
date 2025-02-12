d7c50e820fd9c428a8f52642e8a3ffd410e7dce8
ceochupin <ceochupin@yandex.ru>
2025-02-12 22:38:04 +0300

    rename api.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import './scss/styles.scss';
 import { cloneTemplate, ensureElement } from './utils/utils';
 import { EventEmitter } from './components/base/Events';
 import { API_URL, CDN_URL } from './utils/constants';
-import { Api } from './components/base/iapiap';
+import { Api } from './components/base/api';
 import { IApi, IUser, IUserDataState } from './types';
 import { WebLarekApi } from './components/connector/WebLarekApi';
 
